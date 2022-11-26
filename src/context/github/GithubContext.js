28cb4775bd20e265985907e5445d906502bae6cc
Anmol Gautam <anmol.gautam303@gmail.com>
2022-11-27 03:43:47 +1300

    fix erros

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,11 +1,11 @@
 import { createContext, useState } from "react";
 
-const GithubContext = createContext()
+export const GithubContext = createContext()
 
 const GITHUB_URL =process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
-export const GithubProvider = ({children})=>{
+export const GithubProvider = ({children}) => {
     const [users, setUsers] = useState([])
     const [loading, setLoading] = useState(true)
 
@@ -20,13 +20,11 @@ export const GithubProvider = ({children})=>{
         setUsers(data)
         setLoading(false)
  }
-    return <GithubContext.Provide value={{
+    return <GithubContext.Provider value={{
         users,
         loading,
         fetchUsers,
     }}>
         {children}
-    </GithubContext.Provide>
+    </GithubContext.Provider>
 }
-
-export default GithubContext 
