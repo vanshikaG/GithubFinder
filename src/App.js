35aef6b333a331dfd05239d4e77322fe80f8f169
Anmol Gautam <anmol.gautam303@gmail.com>
2022-11-27 22:29:07 +1300

    add tailwind correctly

--- src/App.js
+++ src/App.js
@@ -10,26 +10,36 @@ import {AlertProvider} from './context/alert/AlertContext'
 
 function App() {
   return (
-   <GithubProvider>
-     <AlertProvider>
-    <Router>
-    <div className = "flex.flex-col.justify-between h-screen">
-    <Navbar/>
-    <main className='container mx-auto px-3 pb-12'>
-     <Alert/>
-     <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/notfound' element={<NotFound />}/>
-        <Route path='/*' element={<NotFound />}/>
-      </Routes>
-    </main>
-    <Footer/>
-    </div>
-   </Router>
-   </AlertProvider>
-   </GithubProvider>
-  );
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
+
+            <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+                <Route
+                  path='/'
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
+                <Route path='/about' element={<About />} />
+                {/*<Route path='/user/:login' element={<User />} />*/}
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='*' element={<NotFound />} />
+              </Routes>
+            </main>
+
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
+  )
 }
 
-export default App;
+export default App
