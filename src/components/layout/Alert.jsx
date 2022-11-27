ba07dddc7982deaff78d58881fa75b1b5cea6a6c
Anmol Gautam <anmol.gautam303@gmail.com>
2022-11-27 21:53:36 +1300

    fix errors

--- src/components/layout/Alert.jsx
+++ src/components/layout/Alert.jsx
@@ -1,11 +1,10 @@
 import {useContext} from 'react'
-import AlertContext from '../../context/alert/AlertContext'
+import {AlertContext} from '../../context/alert/AlertContext'
 
 
 function Alert(){
     const {alert}=useContext(AlertContext)
-    return 
-    alert !== null && (
+    return alert !== null && (
         <p className="flex items-start mb4 space-x-2">
             {alert.type==='error' && (
                   <svg
